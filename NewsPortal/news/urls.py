0b2ebdba1754d77717c75945ace00230f9b8763b
Dimmirion <dimas1995ru@gmail.com>
2025-07-26 18:27:07 +0300

    Add files via upload

--- NewsPortal/news/urls.py
+++ NewsPortal/news/urls.py
@@ -3,11 +3,12 @@ from .views import NewsList, NewsSearch, NewsCreate, ArticleCreate, PostUpdate, 
 
 urlpatterns = [
     path('news/', NewsList.as_view(), name='news_list'),
-    path('news/search/', NewsSearch.as_view(), name='news_search'),
-    path('news/create/', NewsCreate.as_view(), name='news_create'),
-    path('articles/create/', ArticleCreate.as_view(), name='article_create'),
-    path('news/<int:pk>/edit/', PostUpdate.as_view(), name='news_edit'),
+    path('news/', NewsList.as_view(), name='news_list'),          # Список новостей
+    path('news/search/', NewsSearch.as_view(), name='news_search'),  # Поиск
+    path('news/create/', NewsCreate.as_view(), name='news_create'),  # Создание новости
+    path('articles/create/', ArticleCreate.as_view(), name='article_create'),  # Создание статьи
+    path('news/<int:pk>/edit/', PostUpdate.as_view(), name='news_edit'),      # Редактирование
     path('articles/<int:pk>/edit/', PostUpdate.as_view(), name='article_edit'),
-    path('news/<int:pk>/delete/', PostDelete.as_view(), name='news_delete'),
+    path('news/<int:pk>/delete/', PostDelete.as_view(), name='news_delete'),  # Удаление
     path('articles/<int:pk>/delete/', PostDelete.as_view(), name='article_delete'),
 ]
